af475c1d7ae3c80a53c488ff88d79ce34952b6b7
Alejandro del Río <adelriosantiago@gmail.com>
2019-02-22 20:59:07 -0600

    Npm run format

--- boydog-client.js
+++ boydog-client.js
@@ -1,43 +1,43 @@
 "use strict";
 
-const shareDB = require('sharedb/lib/client');
-const stringBinding = require('sharedb-string-binding');
-const reconnectingWebSocket = require('reconnecting-websocket');
-const utils = require('./utils.js');
+const shareDB = require("sharedb/lib/client");
+const stringBinding = require("sharedb-string-binding");
+const reconnectingWebSocket = require("reconnecting-websocket");
+const utils = require("./utils.js");
 
 var boydog = function(client) {
   var scope;
-  
+
   if (!client) client = window.location.host;
-  let socket = new reconnectingWebSocket('ws://' + client);
+  let socket = new reconnectingWebSocket("ws://" + client);
   let connection = new shareDB.Connection(socket);
 
   //Create local Doc instance mapped to 'examples' collection document with id 'textarea'
-  let element = document.querySelector('input');
-  let doc = connection.get('examples', 'randomABC');
+  let element = document.querySelector("input");
+  let doc = connection.get("examples", "randomABC");
   doc.subscribe(function(err) {
     if (err) throw err;
-    
-    let binding = new stringBinding(element, doc, ['content']);
+
+    let binding = new stringBinding(element, doc, ["content"]);
     binding.setup();
   });
-  
+
   /*//Working event example
   doc.on("op", (o, s) => {
     console.log("op", o, s);
   })*/
-  
+
   var reload = function() {
     console.log("reloading boydog", utils);
     utils.normalize();
-  }
-  
+  };
+
   var attach = function(_scope) {
     scope = _scope || "html";
     reload();
-  }
-  
+  };
+
   return { scope, attach };
-}
+};
 
 window.boydog = boydog;
--- utils.js
+++ utils.js
@@ -1,8 +1,14 @@
 "use strict";
 
-const $ = require('cash-dom');
-const _ = require('lodash');
-const allAttributes = ["dog-id", "dog-class", "dog-value", "dog-html", "dog-click"];
+const $ = require("cash-dom");
+const _ = require("lodash");
+const allAttributes = [
+  "dog-id",
+  "dog-class",
+  "dog-value",
+  "dog-html",
+  "dog-click"
+];
 
 //Normalize string like "address.gps.lat" to "address['gps']['lat']" to avoid issues when trying to access fields like "user.2.name"
 var normalizeAttrString = function(attr) {
@@ -14,41 +20,41 @@ var normalizeAttrString = function(attr) {
 
       if (item[0] === "#" || item[0] === ".") return item;
 
-      return `'${ item }'`;
+      return `'${item}'`;
     });
 
-    attr = attr.shift() + `[${ attr.join("][") }]`;
+    attr = attr.shift() + `[${attr.join("][")}]`;
   } else {
     attr = attr.shift();
   }
-  
+
   return attr;
-}
+};
 
 //Get All [dog-value, dog-id, etc] as DOM elements
 var getDogDOMElements = function() {
   let found = {};
-  
-  allAttributes.forEach((attr) => {
-    let el = $(`[${ attr }]`);
+
+  allAttributes.forEach(attr => {
+    let el = $(`[${attr}]`);
     if (el.length === 0) return;
     found[attr] = el;
-  })
-  
+  });
+
   return found;
-}
+};
 
 //Normalize all dog elements paths
 var normalize = function() {
   let els = getDogDOMElements();
-  
-  Object.keys(els).forEach((attrName) => {
+
+  Object.keys(els).forEach(attrName => {
     els[attrName].each((k, el) => {
       let newAttr = normalizeAttrString($(el).attr(attrName));
-      
+
       $(el).attr(attrName, newAttr);
     });
   });
-}
+};
 
 module.exports = { normalize, normalizeAttrString, getDogDOMElements };
