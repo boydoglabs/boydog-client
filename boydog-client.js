afc5cd189a2aa19e288db3f4f4bb5f19ec27574f
Alejandro del Río <adelriosantiago@gmail.com>
2019-02-24 16:45:49 -0600

    Working load 3 elements Add retry if first bind attempt fails

--- boydog-client.js
+++ boydog-client.js
@@ -6,35 +6,62 @@ const reconnectingWebSocket = require("reconnecting-websocket");
 const utils = require("./utils.js");
 
 var boydog = function(client) {
+  var documentScope = {};
   var scope;
 
   if (!client) client = window.location.host;
   let socket = new reconnectingWebSocket("ws://" + client);
   let connection = new shareDB.Connection(socket);
 
-  //Create local Doc instance mapped to 'examples' collection document with id 'textarea'
+  /*//Create local Doc instance mapped to 'examples' collection document with id 'textarea'
   let element = document.querySelector("input");
-  let doc = connection.get("examples", "randomABC");
+  let doc = connection.get("default", "randomABC");
   doc.subscribe(function(err) {
     if (err) throw err;
 
     let binding = new stringBinding(element, doc, ["content"]);
     binding.setup();
-  });
+  });*/
 
   /*//Working event example
   doc.on("op", (o, s) => {
     console.log("op", o, s);
   })*/
 
-  var reload = function() {
-    console.log("reloading boydog", utils);
+  var restart = function() {
+    console.log("reloading boydogxyz", utils);
     utils.normalize();
+    
+    var els = utils.getDogDOMElements();
+    console.log("els", els);
+    
+    let attr = "dog-value";
+    els[attr].each((i, domEl) => {
+      let path = domEl.getAttribute(attr);
+      console.log("domEl", domEl, path);
+      
+      documentScope[path] = connection.get("default", path);
+      documentScope[path].subscribe(function(err) {
+        if (err) throw err;
+
+        let binding = new stringBinding(domEl, documentScope[path], ["content"]);
+        try {
+          binding.setup();
+        } catch(e) {
+          if (e instanceof TypeError) {
+            console.log("retrying to connect");
+            setTimeout(function() {
+              binding.setup();
+            }, 3000);
+          }
+        }
+      });
+    })
   };
 
   var attach = function(_scope) {
     scope = _scope || "html";
-    reload();
+    restart();
   };
 
   return { scope, attach };
